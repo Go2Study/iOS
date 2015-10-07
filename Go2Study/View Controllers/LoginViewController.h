fef18b59b8a11fac04dc602255f40263d5f8bbfb
Ashish Kumar <ashish@ashish.co>
2015-10-07 14:42:44 +0200

    Add Login Button

--- Go2Study/View Controllers/LoginViewController.h
+++ Go2Study/View Controllers/LoginViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  Go2Study
 //
 //  Created by Ashish Kumar on 07/10/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : UIViewController
 
 @end
-
---
+++ Go2Study/View Controllers/LoginViewController.m
@@ -0,0 +1,34 @@
+//
+//  LoginViewController.m
+//  Go2Study
+//
+//  Created by Ashish Kumar on 07/10/15.
+//  Copyright © 2015 Go2Study. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonOAuthLogin;
+
+@end
+
+
+@implementation LoginViewController
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+#pragma mark - Actions
+
+- (IBAction)buttonOAuthLoginPressed:(UIButton *)sender {
+    
+}
+
+
+@end
